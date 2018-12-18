63cd62c8b25e165b0e6f3307b998ed0cf3810edf
Chris Hesik <christopher.hesik@amd.com>
2018-12-18 11:29:32 -0500

    Add CMake file for upcoming DevTools release

---
+++ Global-OpenGL.cmake
@@ -0,0 +1,13 @@
+## Copyright (c) 2018 Advanced Micro Devices, Inc. All rights reserved.
+cmake_minimum_required(VERSION 3.5.1)
+
+set(OpenGLDir ${COMMON_DIR}/Lib/Ext/OpenGL)
+
+set(ADDITIONAL_INCLUDE_DIRECTORIES ${ADDITIONAL_INCLUDE_DIRECTORIES}
+                                   ${OpenGLDir}/Include)
+
+set(ADDITIONAL_LINK_DIRECTORIES ${ADDITIONAL_LINK_DIRECTORIES}
+                                ${OpenGLDir}/Lib/${AMDTPlatform})
+
+set(ADDITIONAL_DEFINTIONS ${ADDITIONAL_DEFINTIONS}
+                          -DOpenGLDir="${OpenGLDir}")
